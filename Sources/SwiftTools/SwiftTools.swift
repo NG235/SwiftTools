ed92e64c75699129f60dfe4637ad73d5be327d36
NG235 <51286292+NG235@users.noreply.github.com>
2020-01-19 12:40:14 +1030

    Create Public Copyright Constant

--- Sources/SwiftTools/SwiftTools.swift
+++ Sources/SwiftTools/SwiftTools.swift
@@ -1,5 +1,9 @@
+import Foundation
+
 public let tools: SwiftTools = SwiftTools()
 
 public struct SwiftTools {
+    
+    public let copyright: String = "(c) NG235 2020. All Rights Reserved."
     
 }
